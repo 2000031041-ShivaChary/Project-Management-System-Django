6b755c284a13f1c1d36d5cc972af6c8c3f4cd536
2000031041-ShivaChary <2000031041@kluniversity.in>
2022-09-04 16:19:18 +0530

    Add existing project files to Git

--- manager/settings.py
+++ manager/settings.py
@@ -91,6 +91,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kumar',
+#         'USER': 'root',
+#         'PASSWORD': 'Shiva_789',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
